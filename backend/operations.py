eec62e1302bb43b6a2149b5cdbe1bdeea6b39a7c
jiri.one <nemec@jiri.one>
2021-10-22 19:20:01 +0200

    added pathlib

--- backend/operations.py
+++ backend/operations.py
@@ -6,7 +6,7 @@ async def copy_one_file(src, dest, chunk_size):
             await dest.write(chunk)
 
 async def copy_file_list(file_list, dest, chunk_size):
-    for src in file_list:
+    for src in file_list: # in the future it can be async too
         async with async_open(src, "rb") as src, async_open(dest, "wb") as dest:
             async for chunk in src.iter_chunked(chunk_size):
                 await dest.write(chunk)
--- tests/test_backend.py
+++ tests/test_backend.py
@@ -5,11 +5,22 @@ import os, sys
 full_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if full_path not in sys.path:
     sys.path.insert(0,full_path)
+# os.chdir(full_path) # switch working directory to EasyFile main directory
 
 # imports needed for testing
 import pytest
 import asyncio
 from aiofile import async_open
+from pathlib import Path
+
+# create files for testing purposes
+fullPath = Path(full_path) # joinpath!!!!!!!!!!!!!!!!!!
+async with async_open("files", "wb") as dest:
+    async for chunk in src.iter_chunked(chunk_size):
+        await dest.write(chunk)
+with open('my_file', 'wb') as f:
+    num_chars = 1024 * 1024 * 1024
+    f.write('0' * num_chars)
 
 # imports of internal functions, which will be tested
 from backend.operations import copy_one_file
