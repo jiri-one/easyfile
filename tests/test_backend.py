460c4cb31a2a6a12352e96ebe7d71fbeb7ae9339
jiri.one <nemec@jiri.one>
2021-10-22 21:24:28 +0200

    small change, now has every created file different content

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -25,9 +25,8 @@ async def main():
     async with async_open(files_path.joinpath("test.file.giga"), "wb") as dest:
         await dest.write(char_to_file * (1024 * 1024 * 1024)) # create 1GB file
     
-    
-    char_to_file = bytes(choice(ascii_letters + punctuation), 'utf-8')
     for file_number in range(1,101):
+        char_to_file = bytes(choice(ascii_letters + punctuation), 'utf-8')
         async with async_open(files_path.joinpath(f"test.file.{file_number}"), "wb") as dest:
             await dest.write(char_to_file * randint(0, 1024 * 1024)) # create 100 files of random size to 1MB     
 
