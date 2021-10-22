fd8726a801d1c684e0c89c695a3e004ef07bf685
jiri.one <nemec@jiri.one>
2021-10-22 19:03:04 +0200

    final preparations for writing tests is done

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,8 +1,21 @@
+# this file contains only testing functions
+
+# I need to add this lines, because I would like to import easily from main directory
+import os, sys 
+full_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if full_path not in sys.path:
+    sys.path.insert(0,full_path)
+
+# imports needed for testing
 import pytest
 import asyncio
 from aiofile import async_open
-from backend import *
+
+# imports of internal functions, which will be tested
+from backend.operations import copy_one_file
 
 # All test coroutines will be treated as marked.
 pytestmark = pytest.mark.asyncio
+
+print(__file__)
 
