4a57be9feb355735c12dfa9a0ac4cd88261e22e2
jiri.one <nemec@jiri.one>
2021-10-24 09:23:42 +0200

    preparation for test is now in tasks and I added default chunk_size

--- backend/operations.py
+++ backend/operations.py
@@ -1,11 +1,11 @@
 from aiofile import async_open
 
-async def copy_one_file(src, dest, chunk_size):
+async def copy_one_file(src, dest, chunk_size: int = 32768):
     async with async_open(src, "rb") as src, async_open(dest, "wb") as dest:
         async for chunk in src.iter_chunked(chunk_size):
             await dest.write(chunk)
 
-async def copy_file_list(file_list, dest, chunk_size):
+async def copy_file_list(file_list, dest, chunk_size: int = 32768):
     for src in file_list: # in the future it can be async too
         async with async_open(src, "rb") as src, async_open(dest, "wb") as dest:
             async for chunk in src.iter_chunked(chunk_size):
--- tests/test_backend.py
+++ tests/test_backend.py
@@ -18,24 +18,35 @@ from random import choice, randint
 # create files for testing purposes
 files_path = Path(full_path).joinpath("files") # path to files directory
 
-async def main():
+async def create_giga_file():
+    """This corutine will create one file named test.file.giga in folder files"""
     char_to_file = bytes(choice(ascii_letters + punctuation), 'utf-8')
     async with async_open(files_path.joinpath("test.file.giga"), "wb") as dest:
         await dest.write(char_to_file * (1024 * 1024 * 1024)) # create 1GB file
-    
+        
+async def create_100_files():
+    """This corutine will create 100 files with random size, every file named test.file.NUM in folder files"""
     for file_number in range(1,101):
+        file_number = str(file_number).zfill(3)
         char_to_file = bytes(choice(ascii_letters + punctuation), 'utf-8')
         async with async_open(files_path.joinpath(f"test.file.{file_number}"), "wb") as dest:
-            await dest.write(char_to_file * randint(0, 1024 * 1024)) # create 100 files of random size to 1MB     
+            await dest.write(char_to_file * randint(0, 1024 * 1024)) # create 100 files of random size to 1MB       
 
+async def main():
+    for task in (create_giga_file, create_100_files):
+        await asyncio.create_task(task())
+  
 asyncio.run(main())
 
 
 # imports of internal functions, which will be tested
-#from backend.operations import copy_one_file
+from backend.operations import copy_one_file
 
 # All test coroutines will be treated as marked.
 pytestmark = pytest.mark.asyncio
+
+async def test_copy_one_file():
+    
 
 print(__file__)
 
