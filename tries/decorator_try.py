9ed864843026a454d98c3015b37f4777e9bb089a
jiri.one <nemec@jiri.one>
2021-10-31 17:47:28 +0100

    some changes in path_handler

--- tries/decorator_try.py
+++ tries/decorator_try.py
@@ -1,14 +1,14 @@
-import functools
-def des(i): # this is the "decorator creator", called with des(1)
-    def deco(func): # this is returned and is the real decorator, called at function definition time
-        @functools.wraps(func) # sugar
+from functools import wraps
+def path_handler(i): # this is the "decorator creator", called with des(1)
+    def helper_decorator(decorated_func): # this is returned and is the real decorator, called at function definition time
+        @wraps(decorated_func) # sugar
         def new_func(*a, **k): # and this is the function called on execution.
             if i == 1:
                 raise Exception # I hope this is just for testing... better create a new exception for this
             else:
                 return func(*a, **k)
         return new_func
-    return deco
+    return helper_decorator
 
 @des(2)
 def func():
