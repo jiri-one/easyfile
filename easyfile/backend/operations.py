bd6fdc555dfb2c254e711761a0705824c50e15c5
jiri.one <nemec@jiri.one>
2022-10-30 15:10:23 +0100

    rename TaskGroup to self._tg

--- easyfile/backend/operations.py
+++ easyfile/backend/operations.py
@@ -24,17 +24,17 @@ class EasyFile:
     async def _copy_path(self, src: Path, dest: Path):
         """Method for asynchronous copying paths (dirs and files) with their content recursively. This function should ideally not be called separately, but always via the "copy" method. If you do call this method, you must ensure that the input parameters are always absolute paths of type Path."""
         if await src.is_file():
-            self.tg.create_task(self._copy_one_file(src, dest / src.name))
+            self._tg.create_task(self._copy_one_file(src, dest / src.name))
         elif await src.is_dir(): 
             new_dest = dest / src.name
-            self.tg.create_task(new_dest.mkdir(exist_ok=True))
+            self._tg.create_task(new_dest.mkdir(exist_ok=True))
             async for one_path in src.iterdir():
                 await self._copy_path(one_path, new_dest)
 
 
     @copy_argument_handler
     async def copy(self, path_list: list[Path | str], dest: Path | str):
-        async with asyncio.TaskGroup() as self.tg:
+        async with asyncio.TaskGroup() as self._tg:
             async for one_path in path_list_to_agen(path_list):
                 await self._copy_path(one_path, dest)
 
